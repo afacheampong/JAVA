7f76690d4c4b4aa7ef44cb45b7660e38c3f62743
afacheampong <afriyieakua3@gmail.com>
2023-05-17 11:28:04 +0000

    JAVA

---
+++ js1.js
@@ -0,0 +1,34 @@
+let name = "Baaba";
+let month = "November";
+let religion = "muslim";
+let food = "Banku";
+
+let number = 3;
+let id = 445;
+let ref = 2259;
+let length = 4;
+let code = 50;
+
+let float1 = 5.2;
+let float2 = 3.4;
+let width = 90.3;
+let height = 45.5
+let float5 = 4.5
+
+let gender = true;
+let feminine = false;
+let student = true;
+let question = true;
+let result = false;
+
+document.write (name);
+document.write (month);
+document.write (religion);
+document.write (food)
+document.write (id);
+document.write (ref);
+document.write (number);
+document.write (float1);
+document.write (gender);
+document.write (student);
+
