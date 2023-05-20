b1e6ee3850cc2f9d5bb4a0c33927cef2d271a8f1
afacheampong <afriyieakua3@gmail.com>
2023-05-20 14:22:32 +0000

    modified to include Arithmetic, Comparison and Logical

---
+++ myscript.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 3;
+let c = 9;
+let d = 405;
+let e = 4;
+
+console.log (10+3);
+console.log (9*3);
+console.log (405%4);
+
+console.log (12 == 12);
+console.log (12<10);
+console.log (-4 >= 4);
+
+console.log (52< 10*2 && 13 >52);
+console.log (14*14 || 10*4);
+console.log (!(14*14) < 10*4);
